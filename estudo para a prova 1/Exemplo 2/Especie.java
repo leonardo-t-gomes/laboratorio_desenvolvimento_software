76756d35adda6f9fea238e31b6b31317447b940a
Leonardo Gomes <leonardo.tgomes@ufn.edu.br>
2026-09-07 20:17:37 -0300

    Create Especie.java

---
+++ estudo para a prova 1/Exemplo 2/Especie.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package source;
+
+/**
+ *
+ * @author User
+ */
+public class Especie {
+    protected String  nomeCientifico;
+    protected String apelido;
+    protected float peso;
+    protected float altura;
+    protected String grupo;
+    protected String sexo;
+
+    public Especie(String nomeCientifico, String apelido, float peso, float altura, String grupo, String sexo) {
+        this.nomeCientifico = nomeCientifico;
+        this.apelido = apelido;
+        this.peso = peso;
+        this.altura = altura;
+        this.grupo = grupo;
+        this.sexo = sexo;
+    }
+
+    public String getNomeCientifico() {
+        return nomeCientifico;
+    }
+
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+}
