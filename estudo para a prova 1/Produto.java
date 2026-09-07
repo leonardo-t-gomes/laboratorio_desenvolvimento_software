5027ed7bcd9b8b12cfdb198e5e5fa88380b3a4d5
Leonardo Gomes <leonardo.tgomes@ufn.edu.br>
2026-09-07 18:38:53 -0300

    Create Produto.java

---
+++ estudo para a prova 1/Produto.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package source;
+
+/**
+ *
+ * @author User
+ */
+public class Produto {
+    protected String nome;
+    protected String codigo;
+    protected float preco;
+    protected float quantidade;
+    protected int ativo;
+    protected String fornecedor;
+
+    public Produto(String nome, String codigo, float preco, float quantidade,int ativo,String fornecedor) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.ativo = ativo;
+        this.fornecedor = fornecedor;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(float quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(int ativo) {
+        this.ativo = ativo;
+    }
+    
+    
+}
